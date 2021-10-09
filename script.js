0f309f67ef3701e01bcceda55126823bd802106a
Jetniphan Pukkham <jeni8399@gmail.com>
2021-10-09 15:19:29 +0700

    candidate-test1

--- script.js
+++ script.js
@@ -3,8 +3,40 @@ $(function() {
         if ($(window).scrollTop() > 50) {
             $(".header").addClass("active-header");
         } else {
-            //remove the background property so it comes transparent again (defined in your css)
             $(".header").removeClass("active-header");
         }
     });
+})
+
+var a = 0;
+$(window).scroll(function() {
+
+    var oTop = $('#timer').offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+        $('.number').each(function() {
+            var $this = $(this),
+                countTo = $this.attr('data-count');
+            $({
+                countNum: $this.text()
+            }).animate({
+                    countNum: countTo
+                },
+
+                {
+
+                    duration: 3000,
+                    easing: 'swing',
+                    step: function() {
+                        $this.text(Math.floor(this.countNum));
+                    },
+                    complete: function() {
+                        $this.text(this.countNum);
+                        //alert('finished');
+                    }
+
+                });
+        });
+        a = 1;
+    }
+
 });
